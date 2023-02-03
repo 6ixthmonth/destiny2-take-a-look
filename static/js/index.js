e36a8566dc27ca27bd806a87a7623f42b6c16f24
June <42332051+6ixthmonth@users.noreply.github.com>
2023-02-03 15:49:53 +0900

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,29 +1,14 @@
 function conn() {
-    // var apiKey = "";
-
-    // $.ajax({
-    //     url: "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/",
-    //     headers: {
-    //         "X-API-Key": apiKey
-    //     }
-    // }).done(function (json) {
-    //     console.log(json.Response.data.inventoryItem.itemName); //Gjallarhorn
-    // });
-
     $.ajax({
         url: "apitest/conntest/",
-        type: "get",
-        // data: {
-
-        // },
         headers: {
-            'X-CSRFTOKEN': '{{ csrf_token }}'
+            "X-CSRFTOKEN": "{{ csrf_token }}"
         },
-        success: function(res) {
-            console.log(res);
-        },
-        error: function(e) {
-            console.log(e);
-        }
+    }).done(function (res) {
+        console.log("success");
+        console.log(res);
+    }).fail(function (res) {
+        console.log("error");
+        console.log(e);
     });
 }
