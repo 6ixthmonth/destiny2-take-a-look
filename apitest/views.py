96f4055e055440ac3371e1789dc5a0523fa69173
June <42332051+6ixthmonth@users.noreply.github.com>
2023-02-06 15:53:45 +0900

    Update views.py

--- apitest/views.py
+++ apitest/views.py
@@ -1,25 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+# import json
 import requests
-import json
 
 def conntest(request):
     # data = json.loads(request.body)
     
-    #dictionary to hold extra headers
     HEADERS = {"X-API-Key":''}
-
-    #make request for Gjallarhorn
-    r = requests.get("https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", headers=HEADERS);
-
-    #convert the json object we received into a Python dictionary object
-    #and print the name of the item
-    inventoryItem = r.json()
+    response = requests.get("https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", headers=HEADERS);
+    inventoryItem = response.json()
     # print(inventoryItem)
     # print(inventoryItem['Response']['data']['inventoryItem']['itemName'])
     itemName = inventoryItem['Response']['data']['inventoryItem']['itemName']
-
-    #Gjallarhorn
 
     return JsonResponse({'itemName': itemName})
