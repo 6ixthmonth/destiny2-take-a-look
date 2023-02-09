184bd301270530f5f46a91905348cece99dcd27c
June <42332051+6ixthmonth@users.noreply.github.com>
2023-02-09 13:09:55 +0900

    Update views.py

--- d2api/views.py
+++ d2api/views.py
@@ -9,13 +9,14 @@ def req(request):
     # data = json.loads(request.body)
 
     HEADERS = {"X-API-Key": settings.API_KEY}
-    membershipType = 0
-    destinyMembershipId = 0
-    characterId = 0
-    vendorHash = 0
-    components = "0"
+    # membershipType = 0
+    # destinyMembershipId = 0
+    # characterId = 0
+    # vendorHash = 0
+    # components = "0"
     # response = requests.get(f"https://www.bungie.net/Platform/Destiny2/{membershipType}/Profile/{destinyMembershipId}/Character/{characterId}/Vendors/?components={components}", headers=HEADERS)
-    response = requests.get(f"https://www.bungie.net/Platform/Destiny2/{membershipType}/Profile/{destinyMembershipId}/Character/{characterId}/Vendors/{vendorHash}/?components={components}", headers=HEADERS)
+    # response = requests.get(f"https://www.bungie.net/Platform/Destiny2/{membershipType}/Profile/{destinyMembershipId}/Character/{characterId}/Vendors/{vendorHash}/?components={components}", headers=HEADERS)
+    response = requests.get(f"https://www.bungie.net/Platform/Destiny2/Vendors/?components=402", headers=HEADERS)
     print(response.json())
 
     return JsonResponse({'key': 'value'})
