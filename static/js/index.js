280cf38fc8b9e497706287009b57c036f4007df2
June <42332051+6ixthmonth@users.noreply.github.com>
2023-02-13 10:41:08 +0900

    Update index.html

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,26 @@
-function req() {
+function getAuth() {
+    alert("{% url 'd2api:get_auth' %}");
     $.ajax({
-        url: "{% url 'req' %}",
-        headers: {
-            "X-CSRFTOKEN": "{{ csrf_token }}"
-        },
+        url: "{% url 'd2api:get_auth' %}"
+    }).done(function (res) {
+        location.href = res['auth_response_url']
+    }).fail(function (e) {
+        console.log(e);
+    });
+}
+
+function req() {
+    // alert(location.search);
+
+    $.ajax({
+        url: "/d2api/req/",
+        data: JSON.stringify({
+            'auth_res': location.search
+        })
     }).done(function (res) {
         console.log("success");
         console.log(res);
-    }).fail(function (res) {
+    }).fail(function (e) {
         console.log("error");
         console.log(e);
     });
