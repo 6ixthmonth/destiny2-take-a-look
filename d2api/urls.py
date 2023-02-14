e4b55d9317bb956e17a1f4b01ab7da5d3c989aa9
June <42332051+6ixthmonth@users.noreply.github.com>
2023-02-14 13:12:19 +0900

    Divide functions

--- d2api/urls.py
+++ d2api/urls.py
@@ -5,6 +5,7 @@ from d2api import views
 app_name = 'd2api'
 urlpatterns = [
     path('getAuth/', views.get_auth, name='get_auth'),
+    path('fetchToken/', views.fetch_token, name='fetch_token'),
     path('refreshToken/', views.refresh_token, name='refresh_token'),
     path('requestData/', views.request_data, name='request_data')
 ]
--- d2api/views.py
+++ d2api/views.py
@@ -1,10 +1,10 @@
 import json
 
+# import requests
 from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from requests_oauthlib import OAuth2Session
-
 
 API_KEY = settings.API_KEY
 CLIENT_ID = settings.CLIENT_ID
@@ -26,15 +26,21 @@ GET_VENDOR_URL = r"/Destiny2/{membershipType}/Profile/{destinyMembershipId}/Char
 
 
 def get_auth(request):
-    auth_session = OAuth2Session(
-        client_id=CLIENT_ID,
-        redirect_uri=REDIRECT_URI
-    )
-    auth_response = auth_session.authorization_url(AUTHORIZATION_URL)
-    # print(f"auth response URL: {auth_response[0]}")
-    # print(f"auth response state: {auth_response[1]}")
+    destiny2 = OAuth2Session(client_id=CLIENT_ID, redirect_uri=REDIRECT_URI)
+    authorization_url, state = destiny2.authorization_url(AUTHORIZATION_URL)
+    request.session['oauth_state'] = state
 
-    return JsonResponse({'auth_response_url': auth_response[0]})
+    return JsonResponse({'authorization_url': authorization_url})
+
+
+def fetch_token(request):
+    data = json.loads(request.body)
+
+    destiny2 = OAuth2Session(client_id=CLIENT_ID, state=request.session['oauth_state'])
+    token = destiny2.fetch_token(token_url=TOKEN_URL, client_secret=CLIENT_SECRET, authorization_response=REDIRECT_URI+data['auth_res'])
+    request.session['oauth_token'] = token
+
+    return JsonResponse(token)
 
 
 def refresh_token(request):
@@ -42,31 +48,20 @@ def refresh_token(request):
 
 
 def request_data(request):
-    data = json.loads(request.body)
-    print(data)
-
-    print(REDIRECT_URI + data['auth_res'])
-
     characterId = TITAN_ID
     destinyMembershipId = DESTINY_MEMBERSHIP_ID
     membershipType = MEMBERSHIP_TYPE
     vendorHash = 69482069  # <Vendor "Commander Zavala">
     components = "400"
 
+    destiny2 = OAuth2Session(client_id=CLIENT_ID, token=request.session['oauth_token'])
     # getVendors
     # endpoint_url = f"https://www.bungie.net/Platform/Destiny2/{membershipType}/Profile/{destinyMembershipId}/Character/{characterId}/Vendors/{vendorHash}/?components={components}"
     # getVendor
     endpoint_url = f"https://www.bungie.net/Platform/Destiny2/{membershipType}/Profile/{destinyMembershipId}/Character/{characterId}/Vendors/{vendorHash}/?components={components}"
-
-    auth_session = OAuth2Session(
-        client_id=CLIENT_ID,
-        redirect_uri=REDIRECT_URI
-    )
-    auth_session.fetch_token(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,
-                             token_url=TOKEN_URL, authorization_response=REDIRECT_URI+data['auth_res'])
-
     headers = {"X-API-Key": API_KEY}
-    response = auth_session.get(url=endpoint_url, headers=headers)
+    # response = requests.get(url=endpoint_url, headers=headers)
+    response = destiny2.get(url=endpoint_url, headers=headers)
 
     print(response.status_code)
     # print(response.text)
