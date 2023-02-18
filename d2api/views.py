64c18ca6d91162e2d06b37a25ab45887619d6149
June <42332051+6ixthmonth@users.noreply.github.com>
2023-02-18 19:54:36 +0900

    Update views.py

--- d2api/views.py
+++ d2api/views.py
@@ -6,14 +6,10 @@ from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from requests_oauthlib import OAuth2Session, TokenUpdated
 
-
 API_KEY = settings.API_KEY
-CLIENT_ID = settings.CLIENT_ID
-CLIENT_SECRET = settings.CLIENT_SECRET
+EXTRA = {'client_id': settings.CLIENT_ID, 'client_secret': settings.CLIENT_SECRET}
 
-TITAN_ID = settings.TITAN_ID
-HUNTER_ID = settings.HUNTER_ID
-WARLOCK_ID = settings.WARLOCK_ID
+CHARACTER_ID_LIST = [settings.TITAN_ID, settings.HUNTER_ID, settings.WARLOCK_ID]
 DESTINY_MEMBERSHIP_ID = settings.DESTINY_MEMBERSHIP_ID
 MEMBERSHIP_TYPE = settings.MEMBERSHIP_TYPE
 
@@ -22,9 +18,41 @@ AUTHORIZATION_URL = "https://www.bungie.net/en/oauth/authorize"
 TOKEN_URL = "https://www.bungie.net/platform/app/oauth/token/"
 BASE_PATH = "https://www.bungie.net/Platform"
 
+VENDOR_HASH_LIST = [
+    69482069,  # Commander Zavala
+    # 248695599,  # The Drifter
+    # 3603221665,  # Lord Shaxx
+]
+VENDOR_ITEM_INDEX_LIST = [
+    [
+        [110, 111, 112, 113, 114],
+        # [50, 51, 52, 53],
+        # [135, 136, 137, 138],
+    ],  # Titan
+    [
+        [105, 106, 107, 108, 109],
+        # [42, 43, 44, 45],
+        # [139, 140, 141, 142],
+    ],  # Hunter
+    [
+        [115, 116, 117, 118, 119],
+        # [46, 47, 48, 49],
+        # [131, 132, 133, 134],
+    ],  # Warlock
+]
+STAT_HASH_LIST = [
+    2996146975,  # Mobility
+    392767087,  # Resilience
+    1943323491,  # Recovery
+    1735777505,  # Discipline
+    144602215,  # Intellect
+    4244567218,  # Strength
+]
+COMPONENTS = "304,402"  # ItemStats, VendorSales
+
 
 def get_auth(request):
-    oauth = OAuth2Session(client_id=CLIENT_ID, redirect_uri=REDIRECT_URI)
+    oauth = OAuth2Session(client_id=EXTRA['client_id'], redirect_uri=REDIRECT_URI)
     authorization_url, state = oauth.authorization_url(url=AUTHORIZATION_URL)
     request.session['oauth_state'] = state
 
@@ -34,9 +62,8 @@ def get_auth(request):
 def fetch_token(request):
     data = json.loads(request.body)
 
-    oauth = OAuth2Session(client_id=CLIENT_ID, state=request.session['oauth_state'])
-    token = oauth.fetch_token(token_url=TOKEN_URL, authorization_response=REDIRECT_URI+data['auth_res'], client_secret=CLIENT_SECRET)
-
+    oauth = OAuth2Session(client_id=EXTRA['client_id'], state=request.session['oauth_state'])
+    token = oauth.fetch_token(token_url=TOKEN_URL, authorization_response=REDIRECT_URI+data['auth_res'], client_secret=EXTRA['client_secret'])
     request.session['oauth_token'] = token
 
     return JsonResponse(token)
@@ -47,31 +74,32 @@ def refresh_token(request):
 
 
 def request_data(request):
-    characterId = TITAN_ID
-    destinyMembershipId = DESTINY_MEMBERSHIP_ID
-    membershipType = MEMBERSHIP_TYPE
-    # vendorHash = 69482069  # Commander Zavala
-    # vendorHash = 248695599  # The Drifter
-    # vendorHash = 3603221665  # Lord Shaxx
-    vendor_hash_list = [69482069, 248695599, 3603221665]
-    components = "402,304"  # ItemStats
+    # repeat for the number of characters
+    for i in range(len(CHARACTER_ID_LIST)):
+        membershipType = MEMBERSHIP_TYPE
+        destinyMembershipId = DESTINY_MEMBERSHIP_ID
+        characterId = CHARACTER_ID_LIST[i]
+        components = COMPONENTS
+        print(f"{characterId=}")
 
-    extra = {'client_id': CLIENT_ID, 'client_secret': CLIENT_SECRET}
-    headers = {"X-API-Key": API_KEY}
-    endpoint_url = f"https://www.bungie.net/Platform/Destiny2/{membershipType}/Profile/{destinyMembershipId}/Character/{characterId}/Vendors/?components={components}"
-    try:
-        oauth = OAuth2Session(client_id=CLIENT_ID, token=request.session['oauth_token'], auto_refresh_url=TOKEN_URL, auto_refresh_kwargs=extra)
-        response = oauth.get(url=endpoint_url, headers=headers)
-    except TokenUpdated as e:
-        print("====================Updated====================")
-        request.session['oauth_token'] = e.token
-        response = oauth.get(url=endpoint_url, headers=headers)
+        for j in range(len(VENDOR_HASH_LIST)):
+            vendorHash = VENDOR_HASH_LIST[j]
+            endpoint_url = f"https://www.bungie.net/Platform/Destiny2/{membershipType}/Profile/{destinyMembershipId}/Character/{characterId}/Vendors/{vendorHash}/?components={components}"
+            headers = {"X-API-Key": API_KEY}
+            try:
+                oauth = OAuth2Session(client_id=EXTRA['client_id'], auto_refresh_url=TOKEN_URL, auto_refresh_kwargs=EXTRA, token=request.session['oauth_token'])
+                response = oauth.get(url=endpoint_url, headers=headers)
+            except TokenUpdated as e:
+                print("==========Token has been updated==========")
+                print(e.token)
+                request.session['oauth_token'] = e.token
+                response = oauth.get(url=endpoint_url, headers=headers)
+            data = response.json()['Response']['itemComponents']['stats']['data']
 
-    print(response.status_code)
-    result = response.json()['Response']['itemComponents']
+            for vendor_item_index in VENDOR_ITEM_INDEX_LIST[i][j]:
+                print(f"\t{vendor_item_index=}")
+                stats = data[str(vendor_item_index)]['stats']
+                for stat_hash in STAT_HASH_LIST:
+                    print(f"\t\t{stat_hash=}, value={stats[str(stat_hash)]['value']}")
 
-    for vendor_hash in vendor_hash_list:
-        pp = pprint.PrettyPrinter(indent=4)
-        pp.pprint(result[str(vendor_hash)]['stats']['data'])
-
-    return JsonResponse(result)
+    return JsonResponse(data)
