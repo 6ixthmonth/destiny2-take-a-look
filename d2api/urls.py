8003d2cd6c935497031b741fa2d82da34f12d1b3
June <42332051+6ixthmonth@users.noreply.github.com>
2023-03-04 15:40:28 +0900

    Update urls.py

--- d2api/urls.py
+++ d2api/urls.py
@@ -5,6 +5,7 @@ from d2api import views
 app_name = 'd2api'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('admin/', views.admin, name='admin'),
     path('getAuth/', views.get_auth, name='get_auth'),
     path('fetchToken/', views.fetch_token, name='fetch_token'),
     path('requestData/', views.request_data, name='request_data')
