409320d5b98c29a9c273ecb384cd587763d1caf1
June <42332051+6ixthmonth@users.noreply.github.com>
2023-03-06 18:21:05 +0900

    Update urls.py

--- d2api/urls.py
+++ d2api/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', views.admin, name='admin'),
     path('getManifest/', views.get_manifest, name='get_manifest'),
-    path('createPickle/', views.create_pickle, name='create_pickle'),
     path('getAuth/', views.get_auth, name='get_auth'),
     path('fetchToken/', views.fetch_token, name='fetch_token'),
     path('getData/', views.get_data, name='get_data')
