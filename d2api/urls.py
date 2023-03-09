9ae4b546ef8d1f4ac4558511e1233c4134e497be
June <42332051+6ixthmonth@users.noreply.github.com>
2023-03-09 13:18:52 +0900

    Update urls.py

--- d2api/urls.py
+++ d2api/urls.py
@@ -4,10 +4,10 @@ from d2api import views
 
 app_name = 'd2api'
 urlpatterns = [
-    path('', views.Home.as_view(), name='home'),
     path('get-manifest/', views.get_manifest, name='get_manifest'),
+    path('get-definition/', views.get_definition, name='get_definition'),
     path('get-auth/', views.get_auth, name='get_auth'),
     path('fetch-token/', views.fetch_token, name='fetch_token'),
-    path('get-data/', views.get_data, name='get_data'),
+    path('get-vendor-data/', views.get_vendor_data, name='get_vendor_data'),
     path('get-limited-time-vendor-Data/', views.get_limited_time_vendor_data, name='get_limited_time_vendor_data'),
 ]
