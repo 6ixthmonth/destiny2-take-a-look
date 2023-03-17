808bf85b7bda8a64e59dd5f3ec39cbd04b684e3f
June <42332051+6ixthmonth@users.noreply.github.com>
2023-03-17 11:06:09 +0900

    Update d2api_extras.py

--- d2api/templatetags/d2api_extras.py
+++ d2api/templatetags/d2api_extras.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter(name='multiply')
 def multiply(value, arg):
     return round(value * arg)
+
+@register.filter(name='percentize')
+def percentize(value):
+    return round(value * 3.33)
