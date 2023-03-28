18e31d85d3747fc9c8583798c856bdd00273afd3
June <42332051+6ixthmonth@users.noreply.github.com>
2023-03-28 09:54:36 +0900

    Update settings.py

--- d2tal/settings.py
+++ d2tal/settings.py
@@ -89,14 +89,21 @@ WSGI_APPLICATION = 'd2tal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env('DB_NAME'),
+#         'USER': env('DB_USER'),
+#         'PASSWORD': env('DB_PASSWORD'),
+#         'HOST': env('DB_HOST'),
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
