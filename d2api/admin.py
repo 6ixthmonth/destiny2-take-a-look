dea883314c8c9cf7992c581f4555274257d8625c
June <42332051+6ixthmonth@users.noreply.github.com>
2023-03-31 13:46:24 +0900

    Update admin.py

--- d2api/admin.py
+++ d2api/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from d2api.models import Item, SalesItem, Vendor
 
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('item_hash', 'item_name')
+
+class SalesItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'item_hash', 'vendor_hash', 'sales_date')
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('vendor_hash', 'vendor_name')
+
 # Register your models here.
-admin.site.register([Item, SalesItem, Vendor])
+admin.site.register(Item, ItemAdmin)
+admin.site.register(SalesItem, SalesItemAdmin)
+admin.site.register(Vendor, VendorAdmin)
