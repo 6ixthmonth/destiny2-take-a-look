337bdaf8d67d19369cd9c23628104af86ace9893
June <42332051+6ixthmonth@users.noreply.github.com>
2023-04-04 09:03:16 +0900

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,20 @@
 window.onload = function () {
-    let page_elements = document.getElementsByClassName("page-link");
+    let page_elements = document.getElementsByClassName('page-link');
     Array.from(page_elements).forEach(function (element) {
         element.addEventListener('click', function () {
             document.getElementById('page').value = this.dataset.page;
-            document.getElementById('searchForm').submit();
+            document.getElementById('filterForm').submit();
         });
+    });
+
+    document.getElementById('filterForm').addEventListener('reset', function() {
+        document.querySelectorAll('a.btn.active').forEach(buttonElement => {
+            const button = bootstrap.Button.getOrCreateInstance(buttonElement);
+            button.toggle();
+        });
+    });
+
+    document.getElementById('filterForm').addEventListener('submit', function() {
+        alert('!');
     });
 };
