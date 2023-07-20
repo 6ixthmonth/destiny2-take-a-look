f745b3251d49750e36086804caf36b68205af019
June <42332051+6ixthmonth@users.noreply.github.com>
2023-07-20 21:23:58 +0900

    Update urls.py

--- d2tal/urls.py
+++ d2tal/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
     path('d2api/', include('d2api.urls')),
+    path('dbapp/', include('dbapp.urls')),
 ]
